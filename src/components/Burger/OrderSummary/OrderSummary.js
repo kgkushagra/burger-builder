0731faa7d9eda0208b4488879d8ba3c3a6364839
kgkushagra <gupta.kushagra3@gmail.com>
2020-06-30 17:37:44 +0530

    added a confirm order dialog box with a backdrop

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+const orderSummary = (props) => {
+    const ingredientSummary=Object.keys(props.ingredients)
+    .map(igKey=>{
+        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}</li>
+    });
+    return (
+        <Aux>
+            <h3>Your order:</h3>
+            <p>Your order with the following ingredients has been confirmed:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+        </Aux>
+    )};
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 const INGREDIENT_PRICES={
     salad:5,
     cheese:4,
@@ -18,7 +19,8 @@ class BurgerBuilder extends Component{
             meat:0,
         },
         totalPrice: 40,
-        purchasable:false
+        purchasable:false,
+        purchasing:false
     }
 
     updatePurchaseState=(ingredients)=>{
@@ -67,6 +69,14 @@ class BurgerBuilder extends Component{
         });
         this.updatePurchaseState(updatedIngredients);
     }
+
+    purchaseHandler=()=>{
+        this.setState({purchasing:true});
+    }
+
+    purchaseCancelHandler=()=>{
+        this.setState({purchasing:false});
+    }
     render(){
         const disabledInfo={
             ...this.state.ingredients
@@ -76,6 +86,9 @@ class BurgerBuilder extends Component{
         }
         return(
             <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
@@ -83,7 +96,7 @@ class BurgerBuilder extends Component{
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
-
+                    ordered={this.purchaseHandler}
                     />
                 <div>Controls</div>
             </Aux>
