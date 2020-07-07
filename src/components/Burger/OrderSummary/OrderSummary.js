1f8bd4abe9f947dc754c0ab7c50916a766eeda1e
kgkushagra <gupta.kushagra3@gmail.com>
2020-07-07 20:16:50 +0530

    change in project structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const ingredientSummary=Object.keys(props.ingredients)
-    .map(igKey=>{
-        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}</li>
+class OrderSummary extends Component{
+    componentDidUpdate()
+        {console.log('[OrderSummary] updated')};
+    render(){
+        const ingredientSummary=Object.keys(this.props.ingredients)
+        .map(igKey=>{
+        return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}</li>
     });
-    return (
-        <Aux>
+        return(
+            <Aux>
             <h3>Your order:</h3>
             <p>Your order with the following ingredients has been confirmed:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price:{props.price}</strong></p>
+            <p><strong>Total Price:{this.props.price}</strong></p>
             <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinue}>Continue</Button>
         </Aux>
-    )};
-export default orderSummary;
+
+        );
+    }
+}
+export default OrderSummary;
