6b6ee45b97202b34a7f934eb73d74749f9f1628a
kgkushagra <gupta.kushagra3@gmail.com>
2020-07-14 17:58:48 +0530

    added a order history page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-
+import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -14,6 +14,7 @@ class App extends Component {
         <Layout>
         <Switch>
            <Route path="/checkout" component={Checkout} />
+           <Route path="/orders" component={Orders}/>
            <Route path="/" exact component={BurgerBuilder} />
 
         </Switch>
---
+++ src/components/Order/Order.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Order.module.css';
+
+const order = (props) => {
+    const ingredients=[];
+    for(let ingredientName in props.ingredients){
+        ingredients.push({name:ingredientName,
+            amount:props.ingredients[ingredientName]
+        });
+    }
+    const ingredientOut=ingredients.map(ig=>{
+        return <span key={ig.name} 
+        style={{textTransform:"capitalize",
+                display:'inline-block',
+                margin:'0 8px',
+                border:'1px solid #ccc',
+                padding:'5px'
+        }}> 
+            {ig.name} ({ig.amount})
+            </span>;
+    })
+    
+    
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients:{ingredientOut} </p>
+            <p>Price:<strong>&#8377;{props.price}</strong></p>
+        </div>
+    );
+};
+
+export default order;
