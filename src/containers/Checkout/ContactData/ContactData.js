50131f1a2f3e083af22659546385cb375eb09c24
kgkushagra <gupta.kushagra3@gmail.com>
2020-07-15 18:59:42 +0530

    added dynamic output of checkout form to backend

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,57 +3,126 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
-    state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalCode:''
-        },loading:false
+    state = {
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },
+        loading: false
     }
-    orderHandler=(event)=>{
+
+    orderHandler = ( event ) => {
         event.preventDefault();
-            this.setState({loading:true});
-       const order={
-           ingredients:this.props.ingredients,
-           price:this.props.price,
-           customer:{
-               name:'Kushagra Gupta',
-               address:{
-                   street:'test',
-                   zipCode:'213123',
-                   country:'India'
-               },
-               email:'test@test.com'
-           },
-           deliveryMethod:'fast'
-       };
-       axios.post('/orders.json',order)
-       .then(response=>{
-           this.setState({loading:false});
-           this.props.history.push('/');
-       })
-       .catch(error=>{
-        this.setState({loading:false});
-       });
+        this.setState( { loading: true } );
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push( '/' );
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
     }
-    render() {
-        let form=(  <form className={classes.Form}>
-            <input className={classes.Input} type='text' name='name' placeholder="Your Name"/>
-            <input className={classes.Input} type='text' name='email' placeholder="Your Mail"/>
-            <input className={classes.Input} type='text' name='street' placeholder="Street"/>
-            <input className={classes.Input} type='text' name='postal' placeholder="Postal Code "/>
-            
-            <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
-        </form>);
-        if(this.state.loading){
-            form=<Spinner/>
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = { 
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form = (
+            <form onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <Button btnType="Success">ORDER</Button>
+            </form>
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
         }
         return (
             <div className={classes.ContactData}>
-                <h4>Enter Contact Details</h4>
-                    {form}
+                <h4>Enter your Contact Data</h4>
+                {form}
             </div>
         );
     }
